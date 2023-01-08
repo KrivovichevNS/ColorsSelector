ce61c3fcb23ed7667086bd685ea7a204c0b49897
NikolayKr <nickdiaz1667@gmail.com>
2023-01-08 17:41:03 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const cols = document.querySelectorAll('.col')
 
+// Смена цвета при нажатии пробела
 document.addEventListener('keydown', event => {
     event.preventDefault()
     if(event.code.toLowerCase() === 'space') {
@@ -7,6 +8,7 @@ document.addEventListener('keydown', event => {
     }
 })
 
+// Открыть/закрыть замочек 
 document.addEventListener('click', event => {
     const type = event.target.dataset.type
 
@@ -17,10 +19,12 @@ document.addEventListener('click', event => {
 
        checkTarget.classList.toggle('fa-lock-open') 
        checkTarget.classList.toggle('fa-lock') 
-
+    } else if (type === 'copy') {
+        copyColorCode(event.target.textContent)
     }
 })
 
+// функция для генерации рандомного цвета (можно заменить методос chroma.random())
 const generateRandomColor = () => {
     const hexCodes = '0123456789ABCDEF'
     let color = ''
@@ -31,10 +35,14 @@ const generateRandomColor = () => {
     return '#' + color
 }
 
+// 'Засеивание колонок рандомными цветами' + (если закрыт замочек, цвет не меняется)
 const setRandomColors = () => cols.forEach(col => {
+    const isLocked = col.querySelector('i').classList.contains('fa-lock')
     const text = col.querySelector('h2')
     const button = col.querySelector('button')
     const color = chroma.random()
+
+    if (isLocked) return
 
     text.textContent = generateRandomColor()
     col.style.background = color
@@ -42,6 +50,11 @@ const setRandomColors = () => cols.forEach(col => {
     setTextColor(text, button, color)
 })
 
+const copyColorCode = (text) => {
+    return navigator.clipboard.writeText(text)
+}
+
+// Изменение цвета шрифа в зависимости от яркости bсg
 const setTextColor = (text, button, color) => {
     const luminance = chroma(color).luminance()
     text.style.color = luminance > 0, 5 ? 'black' : 'white'
